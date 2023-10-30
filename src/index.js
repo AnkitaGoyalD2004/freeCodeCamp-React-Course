a63b6488e497870ef987ab08a7d5b0eee94307b9
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2023-10-30 15:30:46 +0530

    update

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const firstBook = {
-  author: "Francesc Miralles",
-  title: "Intersting facts about Ikigai",
-  img: "./images/ikigai.jpg",
-};
-
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL900_SR900,600_.jpg",
-};
+const books = [
+  {
+    author: "Francesc Miralles",
+    title: "Intersting facts about Ikigai",
+    img: "./images/ikigai.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL900_SR900,600_.jpg",
+    id: 2,
+  },
+];
 
 const thirdBook = {
   author: "Morgan Housel",
@@ -20,31 +23,28 @@ const thirdBook = {
   img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL900_SR900,600_.jpg",
 };
 
+// const names = ["john", "Dishu", "Tashu"];
+// const newNames = names.map((name)=>{
+//    return <h1>{name}</h1>
+// });
+// console.log(newNames);
+
 const BookList = () => {
   return (
     <section className="BookList">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-      <Book
-        author={thirdBook.author}
-        title={thirdBook.title}
-        img={thirdBook.img}
-      />
+      {books.map((books) => {
+        const { img, title, author, id } = book;
+        return <Book img={img} title={title} author={author} key={id} />;
+      })}
     </section>
   );
 };
+//  children Prop
+//everything we want to render in between the component tag  we are using the special prop which is children
 
-const Book = (props) => {
-  console.log(props);
-  const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
+  // console.log(props);
+  // const { img, title, author } = props;
   return (
     <article className="Book">
       <img src={img} alt={title} />
